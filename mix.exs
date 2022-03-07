9838f262f14e4478fdef751b535f3956f282048e
alanraul <raulumb@gmail.com>
2022-03-07 10:31:26 -0600

    fix version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chatex.MixProject do
   def project do
     [
       app:                :chatex,
-      version:            "1.0.0",
+      version:            "1.2.0",
       elixir:             "~> 1.6",
       start_permanent:    Mix.env() == :prod,
       deps:               deps(),
