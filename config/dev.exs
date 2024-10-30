cbd0e138e11bacf7cb0b8061d1fde1e010654533
alanraul <raulumb@gmail.com>
2024-10-30 13:36:21 -0600

    fix to get google_credentials

--- config/dev.exs
+++ config/dev.exs
@@ -5,4 +5,4 @@ config :chatex, token: System.get_env("BOT_TOKEN")
 config :chatex, client_email: System.get_env("BOT_CLIENT_EMAIL")
 
 config :goth,
-  json: System.get_env("GOOGLE_CREDENTIALS") |> File.read!()
+  json: System.get_env("GOOGLE_CREDENTIALS") |> Base.decode64!
